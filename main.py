f9a4e4dfb2cb092b3f87e799802e0e20b6232991
lizard <s-ya98@ya.ru>
2023-02-20 19:37:35 +0300

    path 1.1

--- main.py
+++ main.py
@@ -16,13 +16,44 @@ def command_help(message: telebot.types.Message):
     bot.reply_to(message, f"Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> \
 <в какую валюту надо перевести>\
 <количество первой валюты>\nУвидеть список всех доступных валют: /values")
-
+    text = "Так же есть команда /convert для ступенчатого конвертирования"
+    bot.send_message(message.chat.id, text)
 @bot.message_handler(commands=['values'])
 def command_values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
         text = '\n'.join((text, key, ))
     bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['convert'])
+def values(message: telebot.types.Message):
+    text = 'Выберите валюту, из которой конвертировать:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, base_handler)
+
+def base_handler(message: telebot.types.Message):
+    base = message.text.strip()
+    text = 'Выберите валюту, в которую конвертировать:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, sym_handler, base)
+
+def sym_handler(message: telebot.types.Message, base):
+    sym = message.text.strip()
+    text = 'Выберите сумму конвертации:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, amount_handler, base, sym)
+
+def amount_handler(message: telebot.types.Message, base, sym):
+    amount = message.text.strip()
+    try:
+        new_price = Converter.get_price(base, sym, amount)
+    except APIException as e:
+        bot.send_message(message.chat.id, f'Ошибка конвертации: \n{e}')
+    else:
+        text = f'Цена {amount} {base} в {sym} : {new_price}'
+        bot.send_message(message.chat.id, text)
+
 
 @bot.message_handler(content_types=['text'])
 def converter(message: telebot.types.Message):
